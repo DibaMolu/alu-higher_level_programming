cd28398da6b363558edf8be4777c1e34e1b69d71
DibaMolu <d.molu@alustudent.com>
2023-03-16 06:14:58 -0700

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -18,4 +18,35 @@ class Square:
             raise ValueError("size must be >=0")
         self.__size = size
 
+    def area(self):
+        """Return the area of the square."""
 
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Return the size of the square."""
+        
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+        """
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+     def my_print(self):
+         """Print the square."""
+
+         if self.__size == 0:
+             print()
+         else:
+             for i in range(self.__size):
+                 print("#" * self.__size)
