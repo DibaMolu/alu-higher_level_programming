69b22e78292475b8ef522c5d67e5fc14777d578b
DibaMolu <d.molu@alustudent.com>
2023-03-16 05:42:24 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,39 +2,41 @@
 """Define a class Square."""
 
 
-class Square:
-        """Represent a square."""
+Class Square:
+    """Represent a square."""
 
-            def __init__(self, size=0):
-                        """Initialize a square.
-                                Args:
-                                            size (int): The size of the square.
-                                                    """
-                                                            if isinstance(size, int) is not True:
-                                                                            raise TypeError("size must be an integer")
-                                                                                elif size < 0:
-                                                                                                raise ValueError("size must be >= 0")
-                                                                                                    self.__size = size
+    def __init__(self, size=0):
+        """Initialize a square.
 
-                                                                                                        def area(self):
-                                                                                                                    """Return the area of the square."""
+        Args:
+            size (int): The size of the square.
+        """
+        if isinstance(size, int) is not true:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-                                                                                                                            return self.__size ** 2
+    def area(self):
+        """Return the area of the square."""
 
-                                                                                                                            @property
-                                                                                                                                def size(self):
-                                                                                                                                            """Return the size of the square."""
+        return self.__size ** 2
 
-                                                                                                                                                    return self.__size
+    @property
+    def size(self):
+        """Return to the size of the square."""
 
-                                                                                                                                                    @size.setter
-                                                                                                                                                        def size(self, value):
-                                                                                                                                                                    """Set the size of the square.
-                                                                                                                                                                            Args:
-                                                                                                                                                                                        value (int): The size of the square.
-                                                                                                                                                                                                """
-                                                                                                                                                                                                        if isinstance(value, int) is not True:
-                                                                                                                                                                                                                        raise TypeError("size must be an integer")
-                                                                                                                                                                                                                            elif value < 0:
-                                                                                                                                                                                                                                            raise ValueError("size must be >= 0")
-                                                                                                                                                                                                                                                self.__size = value
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+        """
+        if isinstance(value, int) is not true:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
