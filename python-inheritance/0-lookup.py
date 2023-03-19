0a190179c178f4d67573c66687d1eec84c110002
DibaMolu <d.molu@alustudent.com>
2023-03-19 13:10:56 -0700

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''.defined a method.'''
+""" Funtion that returns list of attributes and methods"""
 
 
 def lookup(obj):
-        '''.instantiated a method.'''
-            return dir((obj))
+    """Returns list of attributes and methods of obj"""
+    return dir(obj)
