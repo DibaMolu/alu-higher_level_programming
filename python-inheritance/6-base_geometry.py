8513a4dd6b2adb80fd8b4aa14c9c50313745d4a7
DibaMolu <d.molu@alustudent.com>
2023-03-19 08:06:24 -0700

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""An empty class"""
+
+
+class BaseGeometry:
+    """Represent a geometry"""
+
+    def area (self):
+        """raise area not implement exception"""
+        raise Exception("area() is not implemented)
