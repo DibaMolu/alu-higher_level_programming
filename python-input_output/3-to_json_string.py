815d6cf71977e5b006a721d816199db8fd36f1b8
DibaMolu <d.molu@alustudent.com>
2023-03-19 09:06:13 -0700

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,5 +5,5 @@ import json
 
 
 def to_json_string(my_obj):
-        """Converts an object to json"""
-            return json.dumps(my_obj)
+    """converts an object to json"""
+    return json.dumps(my_obj)
