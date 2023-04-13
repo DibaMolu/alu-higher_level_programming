305e474f8eb461d50db5f44df9adab62c86747c1
DibaMolu <d.molu@alustudent.com>
2023-04-13 07:33:01 -0700

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ If __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                 JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+                 JOIN states ON cities.states_id = states.id ORDER BY cities.id ASC")
     lst = cur.fetchall()
     for r in lst:
         print(r)
