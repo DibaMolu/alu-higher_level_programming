80f9db0b20d5d6e2efef069b10cf8d53f6ad19e1
DibaMolu <d.molu@alustudent.com>
2023-06-20 00:30:47 -0700

    5-to_integer.js

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+if (isNaN(args[2])) {
+	  console.log('Not a number');
+} else if (Number(args[2]) !== isNaN) {
+	  console.log('My number:', Number(args[2]));
+} else {
+	  console.log('Not a number');
+}
